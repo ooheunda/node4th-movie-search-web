3a57f23032b6a242493ef5ad7293c0dcdfeaeaa5
ooheunda <ooheunda@gmail.com>
2024-01-08 00:55:01 +0900

    검색 기능 event 수정

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
             addIdAlertEvent(movie);
         })
 
-        document.getElementById('searchBtn').addEventListener("click", () => {
+        document.getElementById('searchMovie').addEventListener("keyup", () => {
             searchMovie(movies);
         })
     })
